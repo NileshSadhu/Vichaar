18b4704a4ac6469dc5988536c0f535c04bafe324
nileshsadhu562@gmail.com <nileshsadhu562@gmail.com>
2025-11-25 19:43:24 +0530

    Add Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+const Footer = () => {
+  return (
+    <footer className="py-12 text-center text-sm text-gray-500 font-sans border-t mt-10">
+      <div className="flex justify-center space-x-8 mb-4 font-medium text-gray-800">
+        <a
+          href="https://www.linkedin.com/in/nileshsadhu/"
+          className="hover:text-black transition"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://x.com/nileshsadhu640"
+          className="hover:text-black transition"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://www.instagram.com/x_nilesh_xx/"
+          className="hover:text-black transition"
+        >
+          Instagram
+        </a>
+      </div>
+
+      <p>Copyright © All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
